a3788371ff38e6472e3d3ac74a9533289eb3cf50
Andre Vieira <andrevieira19992018@gmail.com>
2024-09-04 13:43:21 -0300

    Adicionando questão 4

--- TP-3/questao-4/script.js
+++ TP-3/questao-4/script.js
@@ -1,13 +1,11 @@
-let nomeFruta
+let nomeFruta = prompt("Digite o nome de uma fruta: ");
 
-
-
-
-
-// Laranjas, maçãs e bananas:
-// Faça um programa que solicite o nome de uma fruta através de um prompt. Baseado na entrada do usuário, implemente a seguinte lógica:
-// Se usuário digitou "laranja", mostre a mensagem "Não esqueça de descascar!";
-// Se usuário digitou "maça", mostre a mensagem "Se você tirar a casca perderá as vitaminas!";
-// Se usuário digitou "banana", mostre a mensagem "Já vem naturalmente embalada e pronta para o consumo!";
-// Caso o usuário tenha digitado qualquer outra coisa, mostre a mensagem "Não tenho nada a dizer sobre isso.".
-// Utilize if / else / else if para implementar essa lógica!
+if(nomeFruta === "laranja"){
+    alert("Não se esqueça de descascar!");
+} else if (nomeFruta === "maça"){
+    alert("Se você tirar a casca perderá as vitaminas!");
+} else if(nomeFruta === "banana"){
+    alert("Já vem naturalmente embalada e pronta para o consumo!");
+}else{
+    alert("Não tenho nada a dizer sobre isso.");
+}
